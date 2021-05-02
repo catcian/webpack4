fcc17e2b36a97e9bff4973375b3a26913f2d4fc5
wangchen <wangchn86@163.com>
2021-05-02 12:29:37 +0800

    3-9 HMR css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-console.log('hello worl')
-console.log('webpack watch')
-console.log('express server')
+import './index.css'
+
+var button = document.createElement('button')
+button.innerHTML = '新增'
+
+button.onclick = function () {
+  var div = document.createElement('div')
+  div.innerHTML = 'item'
+  document.body.appendChild(div)
+}
+
+document.body.appendChild(button)
