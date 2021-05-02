b871115c95674d2ee670f171096a131415ec0e4d
wangchen <wangchn86@163.com>
2021-05-02 21:07:03 +0800

    4-2 webpack 拆分配置文件

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-
+const webpack = require('webpack')
 
 module.exports = {
-  mode: 'production',
-  devtool: 'cheap-module-source-map',
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    hot: true,
+    hotOnly: true
+  },
   entry: {
     main: './src/index.js',
   },
@@ -63,5 +69,9 @@ module.exports = {
       template: path.resolve(__dirname, 'src/index.html')
     }),
     new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin()
   ],
+  optimization: {
+    usedExports: true
+  }
 }
