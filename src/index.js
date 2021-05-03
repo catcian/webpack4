47e93638d046e98b3a989006428c86e031fa0cdd
wangchen <wangchn86@163.com>
2021-05-03 15:54:21 +0800

    4-4 异步模块引入无需配置自动codeSplitting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import _ from 'lodash'
-var arr = _.join(['a', 'b', 'c'])
-console.log(arr)
+
+function getComponent () {
+  return import('lodash').then(({default: _}) => {
+    var element = document.createElement('div')
+    element.innerHTML = _.join(['a','c','b'])
+    return element
+  })
+}
+
+getComponent().then((element) => {
+  document.body.appendChild(elements)
+})
+
+
+// import _ from 'lodash'
+
+// console.log(_.join(['aaa','bbb']))
