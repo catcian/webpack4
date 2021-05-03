76d56dad0db21447e2176f009961ba7164930fa8
wangchen <wangchn86@163.com>
2021-05-03 22:38:21 +0800

    4-7 Lazy Loading import()

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 
-// function getComponent () {
-//   return import(/* webpackChunkName: "loadsh"*/'lodash').then(({default: _}) => {
-//     var element = document.createElement('div')
-//     element.innerHTML = _.join(['a','c','b'])
-//     return element
-//   })
-// }
-
-// getComponent().then((element) => {
-//   document.body.appendChild(elements)
-// })
+function getComponent () {
+  return import(/* webpackChunkName: "loadsh"*/'lodash').then(({default: _}) => {
+    var element = document.createElement('div')
+    element.innerHTML = _.join(['a','c','b'])
+    return element
+  })
+}
 
 
-// import _ from 'lodash'
-import { info } from './test'
-console.log(JSON.stringify(info))
+document.addEventListener('click', () => {
+  getComponent().then((element) => {
+    document.body.appendChild(element)
+  })
+})
+
