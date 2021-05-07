78f2c8293ca0eb4566c27b7973e32b77e9adceeb
wangchen <wangchn86@163.com>
2021-05-07 22:15:08 +0800

    5-3 lodash&@types/lodash

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
+import * as _ from 'lodash'
+
 class Greeter {
   greeting: string;
   constructor(message: string) {
     this.greeting = message
   }
   greet() {
-    return 'hello' + this.greeting
+    return _.join(['world', this.greeting], ', ')
+    // return 'hello' + this.greeting
   }
 
 }
