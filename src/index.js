1a1f796d78c806db4249fca9aa19dee1fe1c3dc5
wangchen <wangchn86@163.com>
2021-05-14 22:44:05 +0800

    5-6/7 eslint 自定义配置和 webpack 配置

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import axios from 'axios'
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './home'
 import List from './list'
 
 class App extends React.Component {
-
-
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/list" exact component={List}></Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/list" exact component={List} />
         </div>
       </BrowserRouter>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
